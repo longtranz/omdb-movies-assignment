b78c42cce688bedb6041a924a8a75159afae4bbd
Long Tran <tranminhlong88@gmail.com>
2021-11-20 11:28:38 +0700

    Init project

---
+++ OmdbMovies/OmdbMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OmdbMovies
+//
+//  Created by DucNT65.FIN on 11/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
